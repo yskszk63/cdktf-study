0a812b96eb7d372f4dd4cf21c2fb504d0358354b
yskszk63 <yskszk63@gmail.com>
2022-09-11 21:18:02 +0900

    Add iam policy

--- main.ts
+++ main.ts
@@ -1,12 +1,33 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
-import { AwsProvider, s3 } from "@cdktf/provider-aws";
+import { AwsProvider, s3, iam } from "@cdktf/provider-aws";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     new AwsProvider(this, "aws", { region: "ap-northeast-1" });
+
+    const role = new iam.IamRole(this, "lambda-role", {
+      name: "lambda-role",
+      assumeRolePolicy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "sts:AssumeRole",
+            Principal: {
+              Service: "lambda.amazonaws.com",
+            },
+            Effect: "Allow",
+            Sid: "",
+          },
+        ],
+      }),
+    });
+    new iam.IamRolePolicyAttachment(this, "lambda-managed-policy", {
+      policyArn: "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+      role: role.name,
+    });
 
     new s3.S3Bucket(this, "testbucket");
   }
